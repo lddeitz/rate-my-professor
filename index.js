766fc3cdbb1335986f3ad1988db0bfdce6eb5f02
carlhauck <carlhauck@gmail.com>
2020-08-31 19:32:05 -0500

    consolidates professors.js and reviews.js into queries.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dbProfessors = require('./professors');
-const dbReviews = require('./reviews');
+const db = require('./queries');
 const app = express();
 const port = 3000;
